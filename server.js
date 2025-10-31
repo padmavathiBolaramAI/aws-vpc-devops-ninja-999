77a6eab799a9611ca5253c6cc18e7175bdd66bfc
padmavathiBolaramAI <55620565+padmavathiBolaramAI@users.noreply.github.com>
2025-10-31 23:28:54 +0530

    Delete server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Hello from Node.js app on AWS!'));
-app.listen(port, () => console.log(`App running on port ${port}`));
